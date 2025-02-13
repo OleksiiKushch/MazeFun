87a326cf4742c2238707c22dd47faadc879b0a31
Oleksii Kushch <oleksii.kushch@nure.ua>
2025-02-14 01:05:42 +0200

    Added the possibility to copy the map's seed. Updated some styles by changing white to a slightly darker shade.

--- scripts/ui/game-info.js
+++ scripts/ui/game-info.js
@@ -13,7 +13,12 @@ export function createGameInfoUI() {
     <div class="info-content">Steps Taken: <span id="steps-count">0</span></div>
 
     <div class="info-section">Map Info</div>
-    <div class="info-content">Seed: <span id="map-seed">${SEED}</span></div>
+    <div class="info-content">
+      Seed: <span id="map-seed" class="copyable">${SEED}</span>
+      <button id="copy-seed-btn" class="copy-btn" aria-label="Copy Seed">
+        <img src="/resources/copy-icon.svg" alt="Copy" class="copy-icon">
+      </button>
+    </div>
     <div class="info-content">Width: <span id="map-width">${MAP_WIDTH}</span></div>
     <div class="info-content">Height: <span id="map-height">${MAP_HEIGHT}</span></div>
   `;
@@ -21,6 +26,20 @@ export function createGameInfoUI() {
 
   gameInfoButton.addEventListener('click', () => {
     gameInfoModal.classList.toggle('show');
+  });
+
+  document.getElementById('copy-seed-btn').addEventListener('click', function () {
+    const seedText = document.getElementById('map-seed').textContent;
+    navigator.clipboard
+      .writeText(seedText)
+      .then(() => console.log(`The seed of map successfully copied to the clipboard.`))
+      .catch((error) => console.error('Failed to copy the seed of map to the clipboard. Error:', error));
+
+    const button = this;
+    button.classList.add('active');
+    setTimeout(() => {
+      button.classList.remove('active');
+    }, 300);
   });
 }
 
